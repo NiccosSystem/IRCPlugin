eeb499988ffa5c2c569b52867fe46ff1e1bbe4a1
NiccosSystem <niccossystem@gmail.com>
2013-08-02 17:08:42 +0200

    format and cleanup top lel hehehehehhe top contributor

--- src/uk/niccossystem/ircplugin/IRCPlugin.java
+++ src/uk/niccossystem/ircplugin/IRCPlugin.java
@@ -1,32 +1,31 @@
 package uk.niccossystem.ircplugin;
 
 import java.io.IOException;
-
-import org.jibble.pircbot.IrcException;
-
 import net.niccossystem.skypebot.SkypeBot;
 import net.niccossystem.skypebot.plugin.Plugin;
+import org.jibble.pircbot.IrcException;
 
 public class IRCPlugin extends Plugin {
 
-	@Override
-	public String author() {
-		return "NiccosSystem";
-	}
+    @Override
+    public String author() {
+        return "NiccosSystem";
+    }
 
-	@Override
-	public boolean enable() {
-		try {
-			SkypeBot.hooks().registerListener(new ChatListener(this), this);
-		} catch (IOException | IrcException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
+    @Override
+    public boolean enable() {
+        try {
+            SkypeBot.hooks().registerListener(new ChatListener(this), this);
+        }
+        catch (IOException | IrcException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 
-	@Override
-	public String version() {
-		return "0.1";
-	}
+    @Override
+    public String version() {
+        return "0.1";
+    }
 
 }
